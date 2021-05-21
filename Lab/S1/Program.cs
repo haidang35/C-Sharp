e271fe8cf3cc9ef84bf4e7ea493a6d475276118b
Dang Jinner <nguyenhaidangyq@gmail.com>
2021-05-21 16:26:35 +0700

    update

---
+++ Lab/S1/Program.cs
@@ -0,0 +1,27 @@
+﻿using S1.Class;
+
+namespace S1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product prod = new Product(1, "Iphone", 2000, 5, "image1", "SmartPhone");
+            prod.GetInfo();
+            prod.NotificationQuantity();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.AddImageToGallery();
+            prod.DeleteImageInGallery();
+            prod.GetInfo();
+        }
+    }
+}
