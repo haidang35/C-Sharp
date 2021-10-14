144650d92a546646187640ac2515a523fcdde146
Dang Jinner <nguyenhaidangyq@gmail.com>
2021-10-14 15:05:49 +0700

    Updating

---
+++ QualityTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using QualityTest.Controllers;
+using QualityTest.Models;
+
+namespace QualityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateMenu();            
+        }
+
+        static void GenerateMenu()
+        {
+            List<Student> studentList = new List<Student>();
+            var studentController = new StudentController();
+            var option = 1;
+            var isContinue = true;
+            do
+            {
+                Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Create new student");
+                Console.WriteLine("2. Show student list");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Select menu item: ");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        var student = studentController.CreateStudent();
+                        studentList.Add(student);
+                        break;
+                    case 2:
+                        studentController.PrintListStudent(studentList);
+                        break;
+                    case 3:
+                        Console.WriteLine("Goodbye !!");
+                        isContinue = false;
+                        break;
+                    default:
+                        Console.WriteLine("Menu item does not exist, please choose again");
+                        break;
+                }
+            } while ( isContinue);
+
+        }
+
+       
+    }
+}
---
+++ s1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace s1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i;
+            var b = 0;
+            long c = 123_456_789;
+            Console.WriteLine("b= " + c);
+            // string path = "C:\Jinner\hello"; --> cach viet sai
+            string path2 = "C:\\Jinner\\hello"; //--> cach viet dung
+            string path3 = @"C:\JInner\hello"; // correct
+        }
+    }
+}
